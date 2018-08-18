0cb613da5913acdbb3374a3a2109aad57cac8051
吉川雄治 <yuji.yoshikawa219@gmail.com>
2018-08-18 20:56:37 +0900

    ナビゲーション(ヘッダー・フッター)の追加、アイコンの読み込み

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,72 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <v-toolbar fixed>
+      <v-btn icon>
+        <v-icon>arrow_back</v-icon>
+      </v-btn>
+
+      <v-toolbar-title>{{bottomNav}}</v-toolbar-title>
+    </v-toolbar>
+
     <router-view/>
+
+    <v-footer fixed height="102">
+      <v-btn
+        absolute
+        dark
+        fab
+        top
+        right
+        color="teal"
+      >
+        <v-icon>add</v-icon>
+      </v-btn>
+
+      <v-bottom-nav
+        :active.sync="bottomNav"
+        :value="true"
+        absolute
+        color="transparent"
+      >
+        <v-btn
+          color="teal"
+          flat
+          value="recent"
+        >
+          <span>Recent</span>
+          <v-icon>history</v-icon>
+        </v-btn>
+
+        <v-btn
+          color="teal"
+          flat
+          value="favorites"
+        >
+          <span>Favorites</span>
+          <v-icon>favorite</v-icon>
+        </v-btn>
+
+        <v-btn
+          color="teal"
+          flat
+          value="nearby"
+        >
+          <span>Nearby</span>
+          <v-icon>place</v-icon>
+        </v-btn>
+      </v-bottom-nav>
+    </v-footer>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      bottomNav: 'recent'
+    }
+  }
 }
 </script>
 
