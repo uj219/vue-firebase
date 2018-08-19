3412a71dc80ce2a1fedecbc9744fcdea07ae2634
吉川雄治 <yuji.yoshikawa219@gmail.com>
2018-08-19 14:16:48 +0900

    index.vueの要素をコンポーネントに分割

---
+++ src/components/Index/BottomNav.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="bottom-nav">
+    <v-footer fixed height="102">
+      <v-btn
+        absolute
+        dark
+        fab
+        top
+        right
+        color="teal"
+        @click.stop="dialog = true"
+      >
+        <v-icon>add</v-icon>
+      </v-btn>
+
+      <v-bottom-nav
+        :active.sync="bottomNav"
+        :value="true"
+        absolute
+        color="transparent"
+      >
+        <v-btn
+          color="teal"
+          flat
+          value="recent"
+          @click="syncHeader('recent')"
+        >
+          <span>Recent</span>
+          <v-icon>history</v-icon>
+        </v-btn>
+
+        <v-btn
+          color="teal"
+          flat
+          value="favorites"
+          @click="syncHeader('favorites')"
+        >
+          <span>Favorites</span>
+          <v-icon>favorite</v-icon>
+        </v-btn>
+
+        <v-btn
+          color="teal"
+          flat
+          value="nearby"
+          @click="syncHeader('nearby')"
+        >
+          <span>Nearby</span>
+          <v-icon>place</v-icon>
+        </v-btn>
+      </v-bottom-nav>
+    </v-footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BottomNav',
+  data () {
+    return {
+      bottomNav: 'recent'
+    }
+  }
+}
+</script>
---
+++ src/components/Index/List.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="list">
+    <v-container
+      fluid
+      grid-list-md
+    >
+      <v-layout row wrap>
+        <v-flex
+          v-for="card in cards"
+          v-bind="{
+            [`xs${card.flexXs}`]: true,
+            [`sm${card.flexSm}`]: true
+          }"
+          :key="card.title"
+        >
+          <v-card>
+            <router-link :to="`/item/${card.id}`">
+              <v-card-media
+                :src="card.src"
+                height="200px"
+              >
+                <v-container
+                  fill-height
+                  fluid
+                  pa-2
+                >
+                  <v-layout fill-height>
+                    <v-flex xs12 align-end flexbox>
+                      <span class="headline white--text" v-text="card.title"></span>
+                    </v-flex>
+                  </v-layout>
+                </v-container>
+              </v-card-media>
+            </router-link>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn icon>
+                <v-icon>favorite</v-icon>
+              </v-btn>
+              <v-btn icon>
+                <v-icon>bookmark</v-icon>
+              </v-btn>
+              <v-btn icon>
+                <v-icon>share</v-icon>
+              </v-btn>
+            </v-card-actions>
+
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'List'
+}
+</script>
+
+<style scoped>
+</style>
