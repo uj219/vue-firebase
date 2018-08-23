12c640d11c4cf86cf868bc3ef246acec3288e726
吉川雄治 <yuji.yoshikawa219@gmail.com>
2018-08-23 17:55:26 +0900

    検索結果リスト画面のデザイン調整

--- src/components/Index/AddModal/StepperContentList.vue
+++ src/components/Index/AddModal/StepperContentList.vue
@@ -3,8 +3,14 @@
     <v-container>
       <v-layout row wrap>
         <v-flex xs12>
-          <v-list>
+          <v-list two-line>
+            <v-subheader>
+              <v-icon>check</v-icon>
+              追加アイテムの選択
+            </v-subheader>
+
             <v-list-tile
+              class="v-list__tile__custom"
               v-for="item in searchResults"
               :key="item.id"
               @click="confirm(item)"
@@ -23,20 +29,7 @@
               </v-list-tile-action>
             </v-list-tile>
           </v-list>
-
-          <v-divider></v-divider>
         </v-flex>
-
-        <v-btn
-          v-if="hasMoreResults"
-          fab
-          dark
-          color="primary"
-          @click="getMoreItems()"
-        >
-          <v-icon dark>expand_more</v-icon><br>
-          More
-        </v-btn>
 
         <v-flex xs12 d-flex>
           <v-btn
@@ -44,6 +37,14 @@
             @click="changeStep(1)"
           >
             Cancel
+          </v-btn>
+
+          <v-btn
+            v-if="hasMoreResults"
+            color="primary"
+            @click="getMoreItems()"
+          >
+            More
           </v-btn>
         </v-flex>
       </v-layout>
@@ -77,5 +78,8 @@ export default {
 .v-stepper__content {
   padding-right: 0;
   padding-left: 0;
+}
+.v-list__tile__custom {
+  border-bottom: 1px solid rgba(0, 0, 0, 0.16);
 }
 </style>
