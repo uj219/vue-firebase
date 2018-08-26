5db5e7b61267d5a31d2bd25677f4fcf31b8264c9
吉川雄治 <yuji.yoshikawa219@gmail.com>
2018-08-26 18:15:47 +0900

    toolbarのコンポーネント化

---
+++ src/components/common/Toolbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="toolbar">
+    <v-toolbar fixed>
+      <v-btn icon v-if="hasBackLink">
+        <router-link to="/">
+          <v-icon>arrow_back</v-icon>
+        </router-link>
+      </v-btn>
+
+      <v-toolbar-title>{{pageTitle}}</v-toolbar-title>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Toolbar',
+  props: ['pageTitle', 'hasBackLink']
+}
+</script>
